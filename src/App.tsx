1f611e5270f1a744e583987b5e6491682f3f0ba3
infoayraspaces <info.ayraspaces@gmail.com>
2026-02-10 18:54:40 -0500

    Add ScrollToTop to fix navigation scroll

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Community from "./pages/Community";
 import FAQs from "./pages/FAQs";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <HashRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/ubicaciones" element={<Locations />} />
